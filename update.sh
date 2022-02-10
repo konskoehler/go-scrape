7c5495b08a01298ffb3a790e6082ef3c6598bdce
Konstantin Köhler <konstantin.koehler@mhp.com>
2022-02-10 16:41:35 +0100

    add update.sh

---
+++ update.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+REPO=$(aws ecr describe-repositories | jq -r '.repositories[].repositoryUri')
+
+docker build -t $REPO:latest --platform=linux/amd64 .
+docker push $REPO:latest
+
+aws lambda update-function-code \
+    --function-name  go-scrape-lambda \
+    --image-uri $REPO:latest
